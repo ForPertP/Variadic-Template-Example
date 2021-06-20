e0a19aec7bdef16f7f1463723244ec29e4cdced0
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-06-21 01:30:41 +0900

    Update Variadic-Template.cpp

--- Variadic-Template.cpp
+++ Variadic-Template.cpp
@@ -1,16 +1,1 @@
-template <typename T, typename... Args>
-constexpr bool fun_equal(T const& a0, Args const&... args)
-{
-	return ((args == a0) && ...);
-}
 
-int main()
-{
-	cout << boolalpha;
-	cout << fun_equal(1) << '\n';				// true
-	cout << fun_equal(0, 0, 0) << '\n';			// true
-	cout << fun_equal(1, 1, 1, 1, 1) << '\n';	// true
-	cout << fun_equal(1, 2, 3, 4, 5) << '\n';	// false
-    
-	return 0;
-}
